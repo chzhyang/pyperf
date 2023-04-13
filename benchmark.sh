c8df00aeafa7ce5553fc08178d9f305a482516c1
chzhyang <ge.yang@intel.com>
2023-04-12 22:00:15 -0700

    update benchmark for baremetal

--- benchmark.sh
+++ benchmark.sh
@@ -8,8 +8,8 @@
 # Python binary path for pyperformance, use default//usr/bin/python3.11.2...
 python="/home/yangge/pyperformance/python3.11.2/bin/python3.11.2"
 # Workload name, use all/fannkuch...
+workload="fannkuch"
 # workload="chaos"
-workload="chaos"
 # Pinned core, use default/0/1/2/2,10...
 core=2
 
@@ -18,16 +18,16 @@ duration=0
 tps=0
 
 # Enable perf, use true/false
-# perf_enalbed="false"
-perf_enalbed="true"
+perf_enalbed="false"
+# perf_enalbed="true"
 perf_bin="/home/yangge/pyperformance/os.linux.intelnext.kernel/tools/perf/perf"
 # perf delay and duration(seconds)
 perf_delay=5
 perf_duration=10
 
 # Enable emon, use true/false
-# emon_enabled="true"
-emon_enabled="false"
+emon_enabled="true"
+# emon_enabled="false"
 # emon delay and duration(seconds)
 emon_delay=5
 emon_duration=10
@@ -76,11 +76,29 @@ cur_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"
 timestamp=$(date +%Y%m%d%H%M%S)
 result_dir=$cur_dir/$workload-$timestamp
 mkdir $result_dir
+echo result_dir=$result_dir
 # pyperf_result_warmup=$result_dir/$workload-warmup.json
 pyperf_warmup_log=$result_dir/pyperf-warmup-$workload.log
 pyperf_result=$result_dir/$workload.json
 pyperf_log=$result_dir/pyperf-$workload.log
 
+# Print parameters
+touch $result_dir/config.txt
+echo "python=$python" >> $result_dir/config.txt
+echo "workload=$workload" >> $result_dir/config.txt
+echo "core=$core" >> $result_dir/config.txt
+echo "perf_enalbed=$perf_enalbed" >> $result_dir/config.txt
+echo "perf_bin=$perf_bin" >> $result_dir/config.txt
+echo "perf_delay=$perf_delay" >> $result_dir/config.txt
+echo "perf_duration=$perf_duration" >> $result_dir/config.txt
+echo "emon_enabled=$emon_enabled" >> $result_dir/config.txt
+echo "emon_delay=$emon_delay" >> $result_dir/config.txt
+echo "emon_duration=$emon_duration" >> $result_dir/config.txt
+echo "edp_architecture_codename=$edp_architecture_codename" >> $result_dir/config.txt
+echo "edp_architecture_sockets=$edp_architecture_sockets" >> $result_dir/config.txt
+echo "result_dir=$result_dir" >> $result_dir/config.txt
+
+echo "-------------------------------------------------------------------------------"
 echo "Run benchmark first as warm up"
 if [ $core = "default" ]; then
     core_cmd=""
@@ -89,14 +107,12 @@ else
 fi
 
 if [ $workload = "all" ]; then
-    echo "Run all benchmark"
     python3 -m pyperformance run \
         --inherit-environ http_proxy,https_proxy \
         $core_cmd \
         -r \
         -p $python | tee -a ${pyperf_warmup_log}
 else
-    echo "Run benchmark "$workload
     python3 -m pyperformance run \
         --inherit-environ http_proxy,https_proxy \
         $core_cmd \
@@ -104,7 +120,7 @@ else
         -p $python \
         --benchmarks $workload | tee -a ${pyperf_warmup_log}
 fi
-
+echo "-------------------------------------------------------------------------------"
 
 # Extract execution time info from result
 caculate_duration(){
@@ -122,26 +138,28 @@ caculate_duration(){
     duration=$(($end_date - $start_date))
     echo Benchmark duration: $duration secs
     cd $cur_dir
+    echo "-------------------------------------------------------------------------------"
+    echo ""
 }
 
 # Caculate TPS automatically
 caculate_tps(){
     echo "-------------------------------------------------------------------------------"
     echo "Calculating TPS"
+    cd $result_dir
     if [ -f mean.txt ]; then
         rm mean.txt
     fi
     # python3 -m pyperformance show $workload.json | grep 'Mean' > mean.txt
     if [ $workload = "all" ]
     then
-        echo if
         # Collect all workloads mean results
         string_flag="Start date:"
-        last_line=$(sed -n "/$string_flag/=" pyperf-$workload.log | awk '{print $1-1}')
+        last_line=$(sed -n "/$string_flag/=" pyperf-warmup-$workload.log | awk '{print $1-1}')
         echo lateline=$last_line
-        sed -n "1,${last_line}p" pyperf-$workload.log | grep -i ': Mean' > mean.txt
+        sed -n "1,${last_line}p" pyperf-warmup-$workload.log | grep -i ': Mean' > mean.txt
     else
-        cat pyperf-$workload.log | grep -i "$workload: Mean" > mean.txt
+        cat pyperf-warmup-$workload.log | grep -i "$workload: Mean" > mean.txt
     fi
 
     # Move time unit to second from mead.txt to mean-result.txt
@@ -171,18 +189,19 @@ caculate_tps(){
             print(geometric_mean(data))'
     }
     geometric_mean=$(cat mean-result.txt | cut -f2 -d, | geometric_mean)
-    echo "geometric_mean=$geometric_mean secs" >> duration_tps.txt
+    echo "geometric_mean=$geometric_mean secs"
 
     tps=$(echo "scale=2; 1/$geometric_mean" | bc)
     echo "TPS: $tps"
+    cd $cur_dir
+    echo "-------------------------------------------------------------------------------"
 }
 
 # Collect perf data
 collect_perf(){
-    echo "Collect perf data"
     cd $result_dir
-    caculate_duration
-    perf_duration=expr $duration - $perf_delay
+    perf_duration=`expr $duration - $perf_delay`
+    perf_duration=`expr $perf_duration - $perf_delay`
     # $perf_bin script > $result_dir/perf.data
     echo "-------------------------------------------------------------------------------"
     echo "Sleeping for ${perf_delay} secs"
@@ -218,6 +237,7 @@ collect_perf(){
 
 # Collect emon data
 collect_emon(){
+    cd $result_dir
     line="-------------------------------------------------------------------------------"
     echo "Collecting emon data"
     echo ${line}
@@ -275,9 +295,7 @@ process_emon_data(){
     fi
     echo "-------------------------------------------------------------------------------"
     echo "Processing EMON for ${edp_architecture_codename} ${edp_architecture_sockets} and generating CSVs..."
-    caculate_tps
     echo "TPS=$tps"
-    cd $result_dir
     if [ ${edp_architecture_codename} == "cascadelake" ] || [ ${edp_architecture_codename} == "icelake" ] || [ ${edp_architecture_codename} == "sapphirerapids" ]; then
         # CLX, ICX, SPR
         ruby /opt/intel/sep/config/edp/edp.rb --input emon.dat --emonv emon-v.dat --emonm emon-M.dat --dmidecode dmidecode.txt --metric /opt/intel/sep/config/edp/${edp_architecture_codename}_server_${edp_architecture_sockets}_private.xml --format /opt/intel/sep/config/edp/chart_format_${edp_architecture_codename}_server_private.txt --step 1 --socket-view --core-view --end -1 --begin 1 --tps $tps --temp-dir .
@@ -314,7 +332,6 @@ process_emon_data(){
 
 # Read benchmark output, if target string is in the output, start to collect data
 collect_data(){
-    # echo collect data PID, BASHPID, PPID is: $$, $BASHPID, $PPID
     if [ $workload = "all" ]; then
         target_string="[ 1/62] 2to3"
     else
@@ -340,17 +357,17 @@ collect_data(){
     done
 }
 
-
+caculate_duration
+caculate_tps
 benchmark="true"
 touch $pyperf_log
-echo benchmark PID, BASHPID, PPID is: $$, $BASHPID, $PPID
 # ./collect-data.sh $workload $pyperf_log $result_dir > $result_dir/collect.log
 # collect_data > $result_dir/collect.log &
 collect_data &
+echo "-------------------------------------------------------------------------------"
 echo "Run benchmark for data collection"
 if [ $workload = "all" ]
 then
-    echo "Run all benchmark"
     python3 -m pyperformance run \
         --inherit-environ http_proxy,https_proxy \
         $core_cmd \
@@ -358,7 +375,6 @@ then
         -p $python \
         -o $pyperf_result | tee -a ${pyperf_log}
 else
-    echo "Run benchmark "$workload
     python3 -m pyperformance run \
         --inherit-environ http_proxy,https_proxy \
         $core_cmd \
@@ -367,6 +383,7 @@ else
         --benchmarks $workload \
         -o $pyperf_result | tee -a ${pyperf_log}
 fi
+echo "-------------------------------------------------------------------------------"
 
 if [ $emon_enabled = "true" ]; then
     process_emon_data
